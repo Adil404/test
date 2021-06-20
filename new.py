3cac4f41be64356c4d2cadd0691fdc4da1763ad9
Adil <mohdadil3977@gmail.com>
2021-06-20 18:33:29 +0530

    new

--- new.py
+++ new.py
@@ -3,11 +3,11 @@ string = 'saveChangesInTheEditor'
 
 index = 0
 count = 0
+if string[index].islower() == True:
+    count += 1
 for i in range(len(string)):
-    if string[index].islower() == True:
-        count += 1
-    elif string[index + 1].isupper() == True:
+    if string[index].isupper() == True:
         count += 1
         index += 1
 print(count)
-print(len(string))
+print(len(string))    
