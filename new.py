3a643f8a8108835b2831ae284bcf935ecd101d99
Adil <mohdadil3977@gmail.com>
2021-06-20 13:11:30 +0530

    new

--- new.py
+++ new.py
@@ -1,1 +1,13 @@
+string = 'saveChangesInTheEditor'
 
+
+index = 0
+count = 0
+for i in range(len(string)):
+    if string[index].islower() == True:
+        count += 1
+    elif string[index + 1].isupper() == True:
+        count += 1
+        index += 1
+print(count)
+print(len(string))
